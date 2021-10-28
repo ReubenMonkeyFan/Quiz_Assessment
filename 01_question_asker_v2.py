8de329f02ea18e0e2dd0edea6f72e391bb634e72
swan.reube <swan.reube@awatapu.school.nz>
2021-10-29 10:42:21 +1300

    Question asker finished and asking questions correctly

--- 01_question_asker_v2.py
+++ 01_question_asker_v2.py
@@ -1,10 +1,17 @@
 def question_ask(question,answer):
+    error = "Please enter a whole number"
+    while True:
+        try:
+            response = int(input(question))
+            if response == answer:
+                print("Correct")
+                return response
+            else:
+                print("Sorry that is incorrect")
+                return response
+        except ValueError:
+            print(error)
 
-    response = int(input(question))
-    if response == answer:
-        print("Correct")
-    else:
-        print("Sorry that is incorrect")
 
 
 
@@ -12,7 +19,9 @@ def question_ask(question,answer):
 
 
 
-q1 = question_ask("Whats 10 + 5 ", 15)
-q2 = question_ask("Whats 3 x 4 ", 12)
+
+
+q1 = question_ask("Whats 9 + 10? ", 19)
+q2 = question_ask("Whats 3 x 4? ", 12)
 
 
